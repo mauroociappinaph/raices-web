ceaae2360c9c52cc7e43fc9edcb3a5ac189c0af0
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-03-07 10:14:07 -0300

    Refactor App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,17 +5,18 @@ import Footer from "./componentes/footer";
 import Hero from "./componentes/Hero";
 import Nav from "./componentes/Nav";
 import About from "./componentes/About";
+
 function App() {
   return (
-    <>
+    <div className="container mx-auto px-4">
       <Nav />
       <Hero />
       <About />
       <Carrusel />
       <Form />
       <Footer />
-    </>
+    </div>
   );
-}
+  }
 
 export default App;
